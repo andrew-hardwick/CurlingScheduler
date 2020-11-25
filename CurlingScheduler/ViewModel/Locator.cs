b59e155a25ffe73b26e846d75115367e7273885b
Andrew Hardwick <drew.hardwick@gmail.com>
2020-11-25 12:24:48 -0600

    UI Complete

---
+++ CurlingScheduler/ViewModel/Locator.cs
@@ -0,0 +1,26 @@
+﻿using CommonServiceLocator;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Unity;
+using Unity.Lifetime;
+using Unity.ServiceLocation;
+
+namespace CurlingScheduler.ViewModel
+{
+    public class Locator
+    {
+        public Locator()
+        {
+            var container = new UnityContainer();
+
+            container.RegisterType<MainViewModel>(new ContainerControlledLifetimeManager());
+
+            ServiceLocator.SetLocatorProvider(() => new UnityServiceLocator(container));
+        }
+
+        public MainViewModel Main => ServiceLocator.Current.GetInstance<MainViewModel>();
+    }
+}
---
+++ CurlingScheduler/ViewModel/MainViewModel.cs
@@ -0,0 +1,102 @@
+﻿using CurlingScheduler.Service;
+using GalaSoft.MvvmLight;
+using GalaSoft.MvvmLight.Command;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+
+namespace CurlingScheduler.ViewModel
+{
+    public class MainViewModel : ViewModelBase
+    {
+        private RelayCommand _generateSchedule;
+
+        private ObservableCollection<string> _availableDrawAlignment =
+            new ObservableCollection<string>(new string[] { "Balanced", "Squished" });
+
+        private string _drawAlignment = "Squished";
+        private string _teamsText = string.Empty;
+
+        private int _sheetCount = 4;
+        private int _drawCount = 1;
+        private int _drawCountMinimum = 1;
+
+        private IEnumerable<string> _teams;
+        private ScheduleCreator _scheduleCreator;
+
+        public MainViewModel(
+            ScheduleCreator scheduleCreator)
+        {
+            _scheduleCreator = scheduleCreator;
+        }
+
+        public RelayCommand GenerateSchedule => _generateSchedule ?? (_generateSchedule = new RelayCommand(() =>
+        {
+
+            //add code to generate schedule here
+            Console.WriteLine("GenerateSchedule Called");
+        }));
+
+        private void UpdateDrawCountMinimum()
+        {
+            var teamCount = _teams.Count();
+
+            var notEven = teamCount % (2 * SheetCount) != 0;
+
+            DrawCountMinimum = teamCount / (2 * SheetCount) + (notEven ? 1 : 0);
+
+            if (DrawCount < DrawCountMinimum)
+            {
+                DrawCount = DrawCountMinimum;
+            }
+        }
+
+        public ObservableCollection<string> AvailableDrawAlignment
+        {
+            get => _availableDrawAlignment;
+            set => Set(() => AvailableDrawAlignment, ref _availableDrawAlignment, value);
+        }
+
+        public string DrawAlignment
+        {
+            get => _drawAlignment;
+            set => Set(() => DrawAlignment, ref _drawAlignment, value);
+        }
+
+        public string TeamsText
+        {
+            get => _teamsText;
+            set
+            {
+                Set(() => TeamsText, ref _teamsText, value);
+
+                _teams = TeamsText.Split(new[] { Environment.NewLine }, StringSplitOptions.None);
+
+                UpdateDrawCountMinimum();
+            }
+        }
+
+        public int SheetCount
+        {
+            get => _sheetCount;
+            set
+            {
+                Set(() => SheetCount, ref _sheetCount, value);
+                UpdateDrawCountMinimum();
+            }
+        }
+
+        public int DrawCount
+        {
+            get => _drawCount;
+            set => Set(() => DrawCount, ref _drawCount, value);
+        }
+
+        public int DrawCountMinimum
+        {
+            get => _drawCountMinimum;
+            set => Set(() => DrawCountMinimum, ref _drawCountMinimum, value);
+        }
+    }
+}
